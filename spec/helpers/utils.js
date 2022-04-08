3f362b18ba1793a1338f813302aec43a16235765
josephzxy <josephchuh7@gmail.com>
2022-04-08 19:25:37 +0800

    fix: export function sleep in utils

--- spec/helpers/utils.js
+++ spec/helpers/utils.js
@@ -2,7 +2,7 @@
  * sleep sleeps for the given time in milliseconds in async manner
  * @param {number} ms - Time to sleep in milliseconds.
  */
-async function sleep(ms) {
+export async function sleep(ms) {
   await new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
