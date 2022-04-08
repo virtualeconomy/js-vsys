e3a1bd5038e4a59c1865b2bc88196506861ab15d
josephzxy <josephchuh7@gmail.com>
2022-04-08 17:28:40 +0800

    test: fix spec_helper.js

--- spec/helpers/spec_helper.js
+++ spec/helpers/spec_helper.js
@@ -1,9 +1,10 @@
-'use strict'
+'use strict';
 
 import assert from 'assert/strict';
 import * as ut from './utils.js';
+import * as jv from '../../src/index.js';
 
-beforeAll(function() {
+beforeAll(function () {
   this.HOST = process.env.JS_VSYS_HOST;
   // The time in seconds it takes for a block to be packed.
   this.AVG_BLOCK_DELAY = process.env.JS_VSYS_AVG_BLOCK_DELAY;
@@ -11,17 +12,17 @@ beforeAll(function() {
   this.SUPERNODE_ADDR = process.env.JS_VSYS_SUPERNODE_ADDR;
 
   this.api = jv.NodeAPI.new(this.HOST);
-  this.chain = jv.Chain(this.api);
-  this.seed = jv.Seed(this.SEED);
-  this.wallet = jv.Wallet(this.seed);
-  this.acnt0 = this.wallet.getAccount(this.chain, 0);
-  this.acnt1 = this.wallet.getAccount(this.chain, 1);
-  this.acnt2 = this.wallet.getAccount(this.chain, 2);
+  this.chain = new jv.Chain(this.api);
+  this.seed = new jv.Seed(this.SEED);
+  this.wallet = new jv.Wallet(this.seed);
+  this.acnt0 = this.wallet.getAcnt(this.chain, 0);
+  this.acnt1 = this.wallet.getAcnt(this.chain, 1);
+  this.acnt2 = this.wallet.getAcnt(this.chain, 2);
 
   /**
    * waitForBlock waits for the transaction to be packed into a block.
    */
-  this.waitForBlock = async function() {
+  this.waitForBlock = async function () {
     await ut.sleep(this.AVG_BLOCK_DELAY * 1000);
   };
 
@@ -30,7 +31,7 @@ beforeAll(function() {
    * @param {string} txId - The transaction ID.
    * @param {string} status - The status of the transaction.
    */
-  this.assertTxStatus = async function(txId, status) {
+  this.assertTxStatus = async function (txId, status) {
     resp = await this.api.tx.getInfo(txId);
     assert.deepEqual(resp.status, status);
   };
@@ -39,7 +40,7 @@ beforeAll(function() {
    * assertTxSuccess asserts the status of the transaction of the given ID is success.
    * @param {string} txId - The transaction ID.
    */
-  this.assertTxSuccess = async function(txId) {
+  this.assertTxSuccess = async function (txId) {
     await this.assertTxStatus(txId, 'Success');
   };
 
@@ -49,7 +50,7 @@ beforeAll(function() {
    * @param {string} tokId - The token ID.
    * @returns {number} The raw value of the token balance.
    */
-  this.getTokBal = async function(addr, tokId) {
+  this.getTokBal = async function (addr, tokId) {
     const resp = await this.api.ctrt.getTokBal(addr, tokId);
     return resp.balance;
   };
--- spec/helpers/utils.js
+++ spec/helpers/utils.js
@@ -1,10 +1,9 @@
-
 /**
  * sleep sleeps for the given time in milliseconds in async manner
  * @param {number} ms - Time to sleep in milliseconds.
  */
 async function sleep(ms) {
-    await new Promise((resolve) => {
-        setTimeout(resolve, ms);
-    });
+  await new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 }
