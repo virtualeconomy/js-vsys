1ab84125187b8652e50870241be4173f552b4a41
josephzxy <josephchuh7@gmail.com>
2022-04-14 17:30:13 +0800

    feat: add system contract

---
+++ src/contract/sys_ctrt.js
@@ -0,0 +1,186 @@
+/**
+ * module contract/sysCtrt provides functionalities for System Contract.
+ * @module contract/sysCtrt
+ */
+
+'use strict';
+
+import * as ctrt from './ctrt.js';
+import * as acnt from '../account.js';
+import * as md from '../model.js';
+import * as tx from '../tx_req.js';
+import * as de from '../data_entry.js';
+
+/** FuncIdx is the class for function indexes */
+class FuncIdx extends ctrt.FuncIdx {
+  static elems = {
+    SEND: 0,
+    DEPOSIT: 1,
+    WITHDRAW: 2,
+    TRANSFER: 3,
+  };
+  static _ = this.createElems();
+}
+
+/** SysCtrt is the class for System Contract */
+export class SysCtrt extends ctrt.Ctrt {
+  static MAINNET_CTRT_ID = 'CCL1QGBqPAaFjYiA8NMGVhzkd3nJkGeKYBq';
+  static TESTNET_CTRT_ID = 'CF9Nd9wvQ8qVsGk8jYHbj6sf8TK7MJ2GYgt';
+
+  /**
+   * forMainnet returns the SysCtrt instance for mainnet.
+   * @param {ch.Chain} chain - The chain object where the contract is on.
+   * @returns {SysCtrt} The SysCtrt object.
+   */
+  static forMainnet(chain) {
+    return new this(this.MAINNET_CTRT_ID, chain);
+  }
+
+  /**
+   * forTestnet returns the SysCtrt instance for testnet.
+   * @param {ch.Chain} chain - The chain object where the contract is on.
+   * @returns {SysCtrt} The SysCtrt object.
+   */
+  static forTestnet(chain) {
+    return new this(this.TESTNET_CTRT_ID, chain);
+  }
+
+  /**
+   * send sends VSYS coins to another account.
+   * @param {acnt.Account} by - The action taker.
+   * @param {string} recipient - The account address of the recipient.
+   * @param {number} amount - The amount of token to be sent.
+   * @param {string} attachment - The attachment of the action. Defaults to ''.
+   * @param {number} fee - The fee to pay for this action. Defaults to md.ExecCtrtFee.DEFAULT.
+   * @returns {object} The response returned by the Node API.
+   */
+  async send(
+    by,
+    recipient,
+    amount,
+    attachment = '',
+    fee = md.ExecCtrtFee.DEFAULT
+  ) {
+    const rcptMd = new md.Addr(recipient);
+    rcptMd.mustOn(by.chain);
+
+    const data = await by.executeContractImpl(
+      new tx.ExecCtrtFuncTxReq(
+        this.ctrtId,
+        FuncIdx.SEND,
+        new de.DataStack(new de.Addr(rcptMd), de.Amount.forVsysAmount(amount)),
+        md.VSYSTimestamp.now(),
+        new md.Str(attachment),
+        md.ExecCtrtFee.fromNumber(fee)
+      )
+    );
+    return data;
+  }
+
+  /**
+   * transfer transfers tokens from sender to recipient.
+   * @param {acnt.Account} by - The action taker.
+   * @param {string} sender - The account address of the sender.
+   * @param {string} recipient - The account address of the recipient.
+   * @param {number} amount - The amount of token to transfer.
+   * @param {string} attachment - The attachment of the action. Defaults to ''.
+   * @param {number} fee - The fee to pay for this action. Defaults to md.ExecCtrtFee.DEFAULT.
+   * @returns {object} The response returned by the Node API.
+   */
+  async transfer(
+    by,
+    sender,
+    recipient,
+    amount,
+    attachment = '',
+    fee = md.ExecCtrtFee.DEFAULT
+  ) {
+    const senderMd = new md.Addr(sender);
+    senderMd.mustOn(by.chain);
+
+    const rcptMd = new md.Addr(recipient);
+    rcptMd.mustOn(by.chain);
+
+    const data = await by.executeContractImpl(
+      new tx.ExecCtrtFuncTxReq(
+        this.ctrtId,
+        FuncIdx.TRANSFER,
+        new de.DataStack(
+          new de.Addr(senderMd),
+          new de.Addr(rcptMd),
+          de.Amount.forVsysAmount(amount)
+        ),
+        md.VSYSTimestamp.now(),
+        new md.Str(attachment),
+        md.ExecCtrtFee.fromNumber(fee)
+      )
+    );
+    return data;
+  }
+
+  /**
+   * deposit deposits the tokens into the contract.
+   * @param {acnt.Account} by - The action taker.
+   * @param {string} ctrtId - The contract ID to deposit into.
+   * @param {number} amount - The amount of token to deposit.
+   * @param {string} attachment - The attachment of the action. Defaults to ''.
+   * @param {number} fee - The fee to pay for this action. Defaults to md.ExecCtrtFee.DEFAULT.
+   * @returns {object} The response returned by the Node API.
+   */
+  async deposit(
+    by,
+    ctrtId,
+    amount,
+    attachment = '',
+    fee = md.ExecCtrtFee.DEFAULT
+  ) {
+    const data = await by.executeContractImpl(
+      new tx.ExecCtrtFuncTxReq(
+        this.ctrtId,
+        FuncIdx.DEPOSIT,
+        new de.DataStack(
+          new de.Addr(senderMd),
+          de.CtrtAcnt.fromStr(ctrtId),
+          de.Amount.forVsysAmount(amount)
+        ),
+        md.VSYSTimestamp.now(),
+        new md.Str(attachment),
+        md.ExecCtrtFee.fromNumber(fee)
+      )
+    );
+    return data;
+  }
+
+  /**
+   * withdraw withdraws the tokens from the contract.
+   * @param {acnt.Account} by - The action taker.
+   * @param {string} ctrtId - The contract ID to withdraw tokens from.
+   * @param {number} amount - The amount of token to withdraw.
+   * @param {string} attachment - The attachment of the action. Defaults to ''.
+   * @param {number} fee - The fee to pay for this action. Defaults to md.ExecCtrtFee.DEFAULT.
+   * @returns {object} The response returned by the Node API.
+   */
+  async withdraw(
+    by,
+    ctrtId,
+    amount,
+    attachment = '',
+    fee = md.ExecCtrtFee.DEFAULT
+  ) {
+    const data = await by.executeContractImpl(
+      new tx.ExecCtrtFuncTxReq(
+        this.ctrtId,
+        FuncIdx.WITHDRAW,
+        new de.DataStack(
+          new de.Addr(senderMd),
+          de.CtrtAcnt.fromStr(ctrtId),
+          de.Amount.forVsysAmount(amount)
+        ),
+        md.VSYSTimestamp.now(),
+        new md.Str(attachment),
+        md.ExecCtrtFee.fromNumber(fee)
+      )
+    );
+    return data;
+  }
+}
