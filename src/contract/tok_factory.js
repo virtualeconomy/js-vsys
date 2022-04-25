75a69d4eb2711dada1ec0f02f473077ddef3ab40
Meiyunoda <mynoda@connect.hku.hk>
2022-04-25 11:45:09 +0800

    Update tok_factory.js

--- src/contract/tok_factory.js
+++ src/contract/tok_factory.js
@@ -6,10 +6,6 @@
 'use strict';
 
 import * as ctrt from './ctrt.js';
-import * as acnt from '../account.js';
-import * as md from '../model.js';
-import * as tx from '../tx_req.js';
-import * as de from '../data_entry.js';
 import * as en from '../utils/enum.js';
 import * as tok_ctrt_no_split from './tok_ctrt_no_split.js';
 import * as tok_ctrt_split from './tok_ctrt_split.js';
@@ -18,6 +14,7 @@ import * as nft_ctrt from './nft_ctrt.js';
 import * as nft_ctrt_v2 from './nft_ctrt_v2.js';
 import * as sys_ctrt from './sys_ctrt.js';
 
+/** TokCtrtType is the class for token contract types */
 export class TokCtrtType extends en.Enum {
   static elems = {
     NFT : "NonFungibleContract",
@@ -31,30 +28,35 @@ export class TokCtrtType extends en.Enum {
   static _ = this.createElems();
 }
 
-function Map() {
-    this.keys = new Array();
-    this.values= new Array();
 
-
-}
 
 export class TokCtrtMap{
+  Map() {
+    this.keys = new Array();
+    this.values= new ctrt.BaseTokCtrt();
+  }
 
-    static MAP = new Map([
-        [TokCtrtType.NFT, nft_ctrt.NFTCtrt],
-        [TokCtrtType.NFT_V2_BLACKLIST, nft_ctrt_v2.NFTCtrtV2Blacklist],
-        [TokCtrtType.NFT_V2_WHITELIST, nft_ctrt_v2.NFTCtrtV2Whitelist],
-        [TokCtrtType.TOK_NO_SPLIT, tok_ctrt_no_split.TokCtrt],
-        [TokCtrtType.TOK_WITH_SPLIT, tok_ctrt_split.TokCtrtWithSplit],
-        [TokCtrtType.TOK_V2_WHITELIST, tok_ctrt_v2.TokCtrtV2Whitelist],
-        [TokCtrtType.TOK_V2_BLACKLIST, tok_ctrt_v2.TokCtrtV2Blacklist],
-    ]);
+  static MAP = new Map([
+    [TokCtrtType.NFT, nft_ctrt.NFTCtrt],
+    [TokCtrtType.NFT_V2_BLACKLIST, nft_ctrt_v2.NFTCtrtV2Blacklist],
+    [TokCtrtType.NFT_V2_WHITELIST, nft_ctrt_v2.NFTCtrtV2Whitelist],
+    [TokCtrtType.TOK_NO_SPLIT, tok_ctrt_no_split.TokCtrt],
+    [TokCtrtType.TOK_WITH_SPLIT, tok_ctrt_split.TokCtrtWithSplit],
+    [TokCtrtType.TOK_V2_WHITELIST, tok_ctrt_v2.TokCtrtV2Whitelist],
+    [TokCtrtType.TOK_V2_BLACKLIST, tok_ctrt_v2.TokCtrtV2Blacklist],
+  ]);
 
-    static get_tok_ctrt_cls(type) {
-        return this.MAP[type];
-    }
+  static get_tok_ctrt_cls(type) {
+    return this.MAP[type];
+  }
 }
 
+/**
+ * fromTokId creates a token contract instance based on the given token ID.
+ * @param {string} tokId - The token ID.
+ * @param {ch.Chain} chain - The chain.
+ * @returns {TokCtrtType} The token contract instance.
+ */
 async function fromTokId(tokId,chain) {
   if(tokId.isMainnetVsysTok) {
     return sys_ctrt.SysCtrt.forMainnet(chain);
