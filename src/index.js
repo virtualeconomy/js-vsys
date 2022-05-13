89dc139c95869fff140aa302bd2c81f3ff640030
Meiyunoda <mynoda@connect.hku.hk>
2022-05-13 15:52:43 +0800

    fix: add index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+export * from './api.js';
+export * from './chain.js';
+export * from './account.js';
+export * from './tx_req.js';
+export * from './model.js';
+export * as de from './data_entry.js';
+export * from './contract/ctrt.js';
+export * from './contract/lock_ctrt.js';
+export * from './contract/atomic_swap_ctrt.js';
+export * from './contract/nft_ctrt.js';
+export * from './contract/nft_ctrt_v2.js';
+export * from './contract/sys_ctrt.js';
+export * from './contract/tok_ctrt_no_split.js';
+export * from './contract/tok_ctrt_split.js';
+export * from './contract/tok_ctrt_v2.js';
