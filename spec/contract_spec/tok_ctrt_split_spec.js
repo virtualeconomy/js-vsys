e4fde8426aab9f922dbbebae1abbea7940f32ce1
Meiyunoda <mynoda@connect.hku.hk>
2022-05-30 18:20:14 +0800

    fix: use beforeEach

--- spec/contract_spec/tok_ctrt_split_spec.js
+++ spec/contract_spec/tok_ctrt_split_spec.js
@@ -8,36 +8,35 @@
 import * as jv from '../../src/index.js';
 
 describe('Test class TokCtrtWithSplit', function () {
+  beforeEach(async function () {
+    this.tc = await jv.TokCtrtWithSplit.register(this.acnt0, 100, 1);
+    await this.waitForBlock();
+  });
+
   describe('Test method register', function () {
     it('should register an instance of Token Contract with split', async function () {
-      const tc = await jv.TokCtrtWithSplit.register(this.acnt0, 100, 1);
-      await this.waitForBlock();
-
-      const issuer = await tc.getIssuer();
+      const issuer = await this.tc.getIssuer();
       expect(issuer.equal(this.acnt0.addr)).toBeTrue();
 
-      const maker = await tc.getMaker();
+      const maker = await this.tc.getMaker();
       expect(maker.equal(this.acnt0.addr)).toBeTrue();
 
-      const unit = await tc.getUnit();
+      const unit = await this.tc.getUnit();
       expect(unit).toBe(1);
     });
   });
 
   describe('Test method split', function () {
     it('should change to a new unit', async function () {
-      const tc = await jv.TokCtrtWithSplit.register(this.acnt0, 100, 1);
-      await this.waitForBlock();
-
       const maker = this.acnt0;
-      const unitBefore = await tc.getUnit();
+      const unitBefore = await this.tc.getUnit();
       expect(unitBefore).toBe(1);
 
-      const resp = await tc.split(maker, 2);
+      const resp = await this.tc.split(maker, 2);
       await this.waitForBlock();
       await this.assertTxSuccess(resp.id);
 
-      const unitAfter = await tc.getUnit();
+      const unitAfter = await this.tc.getUnit();
       expect(unitAfter).toBe(2);
     });
   });
